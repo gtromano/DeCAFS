634fb3b10dc41b1e8db3709fa77ed4e3db0c2f5b
Gaetano Romano <g.romano@lancaster.ac.uk>
2019-11-19 14:49:54 +0000

    renamed parameter Ni to Nu, minor fixes to .Rmd

--- R/estimateParameters.R
+++ R/estimateParameters.R
@@ -9,8 +9,8 @@ estimateParameters = function(y) {
   cov <- covRob(cbind(yt, yt1, yt2))$cov
   
   phi <- cov[2, 1] / cov[1, 1]
-  sdNi <- cov[2, 1] ^ 2 / (-cov[3, 1] + cov[2, 1])
+  sdNu <- cov[2, 1] ^ 2 / (-cov[3, 1] + cov[2, 1])
   sdEta <- (cov[1,1] * cov[2,1] ^ 2 - cov[1,1] * cov[3,1] ^ 2 + 2 * cov[2,1] ^ 3)/((-cov[3,1] + cov[2,1])*(cov[2,1] + cov[3,1]))
-  return(list(phi = phi, sdEta = sqrt(sdEta), sdNi = sqrt(sdNi)))
+  return(list(phi = phi, sdEta = sqrt(sdEta), sdNu = sqrt(sdNu)))
 }
 

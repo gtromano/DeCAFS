36eb426eb43b3aa82632683fbc6a773d509d0b89
Gaetano Romano <g.romano@lancaster.ac.uk>
2019-11-19 14:09:35 +0000

    Improved description. Added an estimator (to fix).

--- R/estimateParameters.R
+++ R/estimateParameters.R
@@ -1,12 +1,16 @@
+require(robust)
+
 estimateParameters = function(y) {
-  N = length(y)
-  y_0 = y[1:(N - 2)]
-  y_1 = y[2:(N - 1)]
-  y_2 = y[3:(N)]
-  v1 = mad(y_1 - y_0) ^ 2
-  v2 = mad(y_2 - y_0) ^ 2
-  sigmaXsq = abs(v2 - v1)
-  sigmaYsq = abs(v1 - v2/2)
-  return(list(sigmaX = sqrt(sigmaXsq), sigmaY = sqrt(sigmaYsq)))
+  N   <- length(y)
+  yt  <- y[1:(N-2)]
+  yt1 <- y[2:(N-1)]
+  yt2 <- y[3:N]
+  
+  cov <- covRob(cbind(yt, yt1, yt2))$cov
+  
+  phi <- cov[2, 1] / cov[1, 1]
+  sdNi <- cov[2, 1] ^ 2 / (-cov[3, 1] + cov[2, 1])
+  sdEta <- (cov[1,1] * cov[2,1] ^ 2 - cov[1,1] * cov[3,1] ^ 2 + 2 * cov[2,1] ^ 3)/((-cov[3,1] + cov[2,1])*(cov[2,1] + cov[3,1]))
+  return(list(phi = phi, sdEta = sqrt(sdEta), sdNi = sqrt(sdNi)))
 }
 

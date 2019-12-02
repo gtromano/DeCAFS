77defc2e4c54c3e24f75c35020d14521871e408c
Gaetano Romano <g.romano@lancaster.ac.uk>
2019-12-02 11:56:35 +0000

    returning signal in the data generation function

--- R/generateRWAR.R
+++ R/generateRWAR.R
@@ -36,5 +36,5 @@ dataRWAR <- function(n = 1e3, poisParam = 0.01, meanGap = 10, phi = .98, sdEta =
   mu = f + g
   epsilon <- .dataAR_c(phi, rnorm(1, 0, sdNu/sqrt(1 - phi^2)), mu, rnorm(n, sd = sdNu))$z
   y = epsilon
-  return(list(y = y, f = f, changepoints = which(changepoints > 0)))
+  return(list(y = y, signal = mu, changepoints = which(changepoints > 0)))
 }

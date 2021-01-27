75b09aacafb3a7b5428587f28aa83c582e8575ab
gtromano <g.romano@lancaster.ac.uk>
2021-01-27 22:07:02 +0100

    updated simulations

--- simulations/additional_simulations/ARIndep.R
+++ simulations/additional_simulations/ARIndep.R
@@ -1,34 +1,35 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-#                                                                       #
-#   A minor simulation on the Ornstein - Uhlenbeck process              #
-#   with changes                                                        #
-#                                                                       #
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                                                        #
+#   AR w\ independence across various segments           #
+#                                                        #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
 
 library(tidyverse)
 library(parallel) # for mclapply
 library(DeCAFS)
-library(AR1seg) # not available for R 4.x
+library(AR1seg)
 
 source("simulations/helper_functions.R")
 
-# data generating function
-dataOrnsteinUhlenbech <- function (n = 1e3, y0 = 0, theta = 0, sdEta = 1, sdNu = 1, sdEpsilon = 1, type = c("none", "up", "updown", "rand1"), nbSeg = 20, jumpSize = 10)  {
+
+dataARIndep <- function (n = 1e3, phi = 0, sdNu = 1, type = c("none", "up", "updown", "rand1"), nbSeg = 20, jumpSize = 10)  {
 
   f <- scenarioGenerator(n, type = type, nbSeg = nbSeg, jumpSize = jumpSize)
-  dw  <- rnorm(n, 0, sdEta)
+  changepoints <- which(diff(f) != 0)
 
-  nu <- vector(length = n, mode = "double")
-  nu[1] <- rnorm(1, 0, sdNu^2/(1-theta^2) )
-  for (t in 2:n) {
-    nu[t] <-  nu[t-1] - theta * nu[t-1] + sdNu * dw[t-1]
+  out <- NULL
+
+  for (seglen in diff(c(0, changepoints, n))) {
+    y <- dataRWAR(n = seglen, phi = phi, sdNu = 2, sdEta = 0)$y
+    out <- c(out, y)
   }
 
-  signal <- f + nu
-  y <- f + nu + sdEpsilon
+  out <- out + f
 
-  return(list(y = y, signal = signal, changepoints = which(diff(f) != 0)))
+  return(list(y = out, signal = f, changepoints = changepoints))
 }
+
 
 
 # simulation settings
@@ -37,37 +38,29 @@ N <- 5e3 # lenght of the sequence
 CORES <- 6
 
 # generate a list of simulations
-simulations <- expand.grid(sigmaEta = 1,
-                           sigmaNu = 1,
-                           theta = seq(0, .99, length.out = 8),
-                           scenario = c("none", "up", "updown", "rand1"),
-                           jumpSize = 5)
+simulations <- expand.grid(sigmaNu = 2,
+                           phi = seq(0, .95, length.out = 8),
+                           scenario = c("up", "updown", "rand1"),
+                           jumpSize = 10)
 
 
 ##### FUNCTION FOR RUNNING SIMULATIONS ####
 runSim <- function(i, simulations) {
   # here we save the simulation
-  fileName <- paste(c("simulations/additional_simulations/resOU/", simulations[i, ], ".RData"), collapse = "")
+  fileName <- paste(c("simulations/additional_simulations/resARIndep/", simulations[i, ], ".RData"), collapse = "")
   # if file already exist, do not run
   if (!file.exists(fileName)) {
     cat("Running ", fileName, "\n")
     p <- simulations[i, ]
 
-    Y <- mclapply(1:REPS, function(r) dataOrnsteinUhlenbech(N, theta = p$theta, sdEta = p$sigmaEta, sdNu = p$sigmaNu,  jumpSize = p$jumpSize, type = as.character(p$scenario)), mc.cores = CORES)
+    Y <- mclapply(1:REPS, function(r) dataARIndep(N, phi = p$phi, sdNu = p$sigmaNu, jumpSize = p$jumpSize, type = as.character(p$scenario)), mc.cores = CORES)
 
     signal <- lapply(Y, function(r) r$signal)
     y <- lapply(Y, function(r) r$y)
     changepoints <- Y[[1]]$changepoints
 
-    # DeCAFS K 15
-    if (p$sigmaEta == 0)
-      resDeCAFSESTK15 <- mclapply(y, function(y){
-        est <- estimateParameters(y, sdEtaUpper = .0001)
-        est$sdEta <- 0
-        DeCAFS(y, beta = 2 * log(N), modelParam = est)
-      }, mc.cores = CORES)
-    else
-      resDeCAFSESTK15 <- mclapply(y, DeCAFS, mc.cores=CORES)
+    #DeCAFS K 15
+    resDeCAFSESTK15 <- mclapply(y, DeCAFS, mc.cores = CORES)
 
     # ar1seg with estimator
     resar1segEST <- mclapply(y, AR1seg_func, Kmax = 40, mc.cores=CORES)
@@ -85,10 +78,6 @@ runSim <- function(i, simulations) {
 }
 
 
-
-
-##### RUNNING SIMULATIONS FOR RANGING THETA #####
-
 # selecting the relevant simulations
 toSummarize <- simulations
 
@@ -96,18 +85,20 @@ toSummarize <- simulations
 if (T) lapply(1:nrow(toSummarize), runSim, simulations = toSummarize)
 
 
+
+
 # generate the F1 dataset
 F1df <- mclapply(1:nrow(toSummarize), function(i) {
   p <- toSummarize[i, ]
   print(p)
 
-  fileName <- paste(c("simulations/additional_simulations/resOU/", p, ".RData"), collapse = "")
+  fileName <- paste(c("simulations/additional_simulations/resARIndep/", p, ".RData"), collapse = "")
   if (!file.exists(fileName)) {
     cat("Missing", paste0(Map(paste, names(p), p), collapse = " "), "\n")
     return(NULL)
   } else load(fileName)
 
-  AR1segdfest <- cbind(p$theta,
+  AR1segdfest <- cbind(p$phi,
                        sapply(resar1segEST, function(r)
                          computeF1Score(c(changepoints, N), r$PPSelectedBreaks, 3)) %>% as.numeric, # comptuting the F1
                        sapply(resar1segEST, function(r)
@@ -116,7 +107,7 @@ F1df <- mclapply(1:nrow(toSummarize), function(i) {
                          computeRecall(c(changepoints, N), r$PPSelectedBreaks, 3)) %>% as.numeric, # comptuting the F1
                        as.character(p$scenario),
                        "AR1Seg est")
-  DeCAFSdfK15 <- cbind(p$theta,
+  DeCAFSdfK15 <- cbind(p$phi,
                        sapply(resDeCAFSESTK15, function(r) computeF1Score(c(changepoints, N), c(r$changepoints, N), 3)) %>% as.numeric,
                        sapply(resDeCAFSESTK15, function(r) computePrecision(c(changepoints, N), c(r$changepoints, N), 3)) %>% as.numeric,
                        sapply(resDeCAFSESTK15, function(r) computeRecall(c(changepoints, N), c(r$changepoints, N), 3)) %>% as.numeric,
@@ -129,8 +120,8 @@ F1df <- mclapply(1:nrow(toSummarize), function(i) {
 
 
 F1df <- Reduce(rbind, F1df)
-colnames(F1df) <- c("theta", "F1Score", "Precision", "Recall", "Scenario", "Algorithm")
-F1df <- as_tibble(F1df) %>% mutate(theta = as.numeric(theta),
+colnames(F1df) <- c("phi", "F1Score", "Precision", "Recall", "Scenario", "Algorithm")
+F1df <- as_tibble(F1df) %>% mutate(phi = as.numeric(phi),
                                    F1Score = as.numeric(F1Score),
                                    Precision = as.numeric(Precision),
                                    Recall = as.numeric(Recall))
@@ -138,36 +129,33 @@ F1df <- as_tibble(F1df) %>% mutate(theta = as.numeric(theta),
 
 cbPalette3 <- c("#56B4E9",  "#33cc00", "#434242", "#542354")
 scores <- ggplot(F1df,
-                 aes(x = theta, y = F1Score, group = Algorithm, by = Algorithm, col = Algorithm)) +
+                 aes(x = phi, y = F1Score, group = Algorithm, by = Algorithm, col = Algorithm)) +
   stat_summary(fun.data = "mean_se", geom = "line") +
   stat_summary(fun.data = "mean_se", geom = "errorbar", width = .05) +
   facet_wrap(~ Scenario) +
   scale_color_manual(values = cbPalette3) +
-  xlab(expression(italic(theta)))
+  xlab(expression(italic(phi)))
 
 scores
-ggsave(scores, width = 6, height = 4, units = "in", file = "simulations/outputs/OUF1.pdf", device = "pdf", dpi = "print")
+ggsave(scores, width = 6, height = 4, units = "in", file = "simulations/outputs/ARIndepF1.pdf", device = "pdf", dpi = "print")
 
 
-Prec <- ggplot(F1df,
-                 aes(x = theta, y = Precision, group = Algorithm, by = Algorithm, col = Algorithm)) +
+scores <- ggplot(F1df,
+                 aes(x = phi, y = Precision, group = Algorithm, by = Algorithm, col = Algorithm)) +
   stat_summary(fun.data = "mean_se", geom = "line") +
   stat_summary(fun.data = "mean_se", geom = "errorbar", width = .05) +
   facet_wrap(~ Scenario) +
   scale_color_manual(values = cbPalette3) +
-  xlab(expression(italic(theta)))
+  xlab(expression(italic(phi)))
 
-Prec
-ggsave(Prec, width = 6, height = 4, units = "in", file = "simulations/outputs/OUPrec.pdf", device = "pdf", dpi = "print")
+ggsave(scores, width = 6, height = 4, units = "in", file = "simulations/outputs/ARIndepPrec.pdf", device = "pdf", dpi = "print")
 
-
-Rec <- ggplot(F1df,
-                 aes(x = theta, y = Recall, group = Algorithm, by = Algorithm, col = Algorithm)) +
+scores <- ggplot(F1df,
+                 aes(x = phi, y = Recall, group = Algorithm, by = Algorithm, col = Algorithm)) +
   stat_summary(fun.data = "mean_se", geom = "line") +
   stat_summary(fun.data = "mean_se", geom = "errorbar", width = .05) +
   facet_wrap(~ Scenario) +
   scale_color_manual(values = cbPalette3) +
-  xlab(expression(italic(theta)))
+  xlab(expression(italic(phi)))
 
-Rec
-ggsave(Rec, width = 6, height = 4, units = "in", file = "simulations/outputs/OURec.pdf", device = "pdf", dpi = "print")
+ggsave(scores, width = 6, height = 4, units = "in", file = "simulations/outputs/ARIndepRecall.pdf", device = "pdf", dpi = "print")
